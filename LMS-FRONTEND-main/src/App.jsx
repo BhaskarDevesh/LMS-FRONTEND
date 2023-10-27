1db22e818ef3360b0ec2cbe446a75501fbfb08f4
BhaskarDevesh <bhaskardevesh@gmail.com>
2023-10-27 23:54:58 +0530

    learning management system

---
+++ LMS-FRONTEND-main/src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from './Components/Footer';
+
+import { Routes } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+      {/* <Routes> */}
+      {/* <Routes> */}
+        {/* <Route path="/" element={<Home />} ></Route> */}
+      {/* </Routes> */}
+      {/* </Routes> */}
+        <HomeLayout />
+    </>
+  )
+}
+
+export default App
